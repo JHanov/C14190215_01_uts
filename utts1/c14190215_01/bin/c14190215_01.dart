78ce3e7f3fd19861d12e2139bd29cd8cb2b6fd81
JHanov <hanovjeremy@gmail.com>
2022-04-07 15:55:39 +0700

    ok

--- utts1/c14190215_01/bin/c14190215_01.dart
+++ utts1/c14190215_01/bin/c14190215_01.dart
@@ -1,3 +1,50 @@
+import 'dart:io';
+
 void main(List<String> arguments) {
-  print('Hello world!');
+  print("Kata : ");
+  String? kata = stdin.readLineSync();
+  print("Jumlah : ");
+  String? jumlah = stdin.readLineSync();
+  int Jumlah = int.parse(jumlah!);
+
+  int? stg = (Jumlah % 2) + (Jumlah ~/ 2);
+  int ctr = 0;
+
+  if (Jumlah != null) {
+    for (int i = 0; i <= stg; i++) {
+      for (int j = stg; j > i; j++) {
+        stdout.write(" ");
+      }
+      for (int j = 0; j < (2 * i - 1); j++) {
+        if (j == 0 || j == (2 * i - 1) - 1) {
+          stdout.write("+");
+        } else {
+          if (ctr == kata!.length) {
+            ctr = 0;
+          }
+          stdout.write(kata[ctr]);
+          ctr++;
+        }
+      }
+      stdout.write("\n");
+    }
+
+    for (int i = stg - 1; i >= 0; i--) {
+      for (int j = stg; j > 1; j--) {
+        stdout.write(" ");
+      }
+      for (int j = 0; j < (2 * i - 1); j++) {
+        if (j == 0 || j == (2 * i - 1) - 1) {
+          stdout.write("+");
+        } else {
+          if (ctr == kata!.length) {
+            ctr = 0;
+          }
+          stdout.write(kata[ctr]);
+          ctr++;
+        }
+      }
+      stdout.write("\n");
+    }
+  }
 }
